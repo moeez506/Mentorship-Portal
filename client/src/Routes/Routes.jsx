d5bd3b3dc8f2cd30eaa3b8b1d5df346865490899
Haris Baig <bsf2100818@ue.edu.pk>
2024-04-21 21:44:55 +0500

    Delete client directory

--- client/src/Routes/Routes.jsx
+++
@@ -1,8 +0,0 @@
-import SignUpPage from "../pages/SignUpPage";
-import LoginPage from "../pages/LoginPage";
-import HomePage from "../pages/HomePage";
-import ProfilePage from "../pages/ProfilePage";
-import TaskPage from "../components/Roadmap/TaskPage";
-import RoadmapPage from "../components/Roadmap/RoadmapPage";
-
-export { SignUpPage, LoginPage, HomePage, ProfilePage, TaskPage, RoadmapPage };
