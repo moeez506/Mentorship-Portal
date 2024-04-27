048cf74543c895c5beae5f0749183045167c8ca2
Haris Baig <bsf2100818@ue.edu.pk>
2024-04-27 17:59:02 +0500

    Profile and Requests UI Done

---
+++ client/src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import SignUpPage from "../pages/SignUpPage";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import ProfilePage from "../pages/ProfilePage";
+import TaskPage from "../components/Roadmap/TaskPage";
+import RoadmapPage from "../components/Roadmap/RoadmapPage";
+import RequestPage from "../components/Requests/RequestsPage";
+
+export {
+  SignUpPage,
+  LoginPage,
+  HomePage,
+  ProfilePage,
+  TaskPage,
+  RoadmapPage,
+  RequestPage,
+};
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Login from "../components/Auth/Login";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
