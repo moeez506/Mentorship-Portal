e53c30ad03c4af7a12c00e7ae9fc1f72ba848672
Haris Baig <bsf2100818@ue.edu.pk>
2024-05-01 20:41:24 +0500

    Removed Extra Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Hero from "./components/Route/Hero/Hero";
 import Navbar from "./components/Layout/Navbar";
-import {
-  LoginPage,
-  ProfilePage,
-  RequestPage,
-  RoadmapPage,
-  SignUpPage,
-  TaskPage,
-  MentorsList,
-  StudentsList,
-  Hero,
-} from "./Routes/Routes";
+import Sidebar from "./components/Layout/Sidebar";
+import Login from "./components/Auth/Login";
+import SignUp from "./components/Auth/SignUp";
+import RoadmapPage from "./components/Roadmap/RoadmapPage";
+import TaskPage from "./components/Roadmap/TaskPage";
+import RequestsPage from "./components/Requests/RequestsPage";
+import Profile from "./components/Profile/Profile";
+import MentorsList from "./components/List/MentorsList";
+import StudentsList from "./components/List/StudentsList";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Hero />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/roadmaps" element={<RoadmapPage />} />
-          <Route path="/requests" element={<RequestPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
           <Route path="/task/:id" element={<TaskPage />} />
+          <Route path="/requests" element={<RequestsPage />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/mentors" element={<MentorsList />} />
           <Route path="/students" element={<StudentsList />} />
         </Routes>
--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -1,18 +1,76 @@
-import React, { useState } from "react";
-import { FaBars } from "react-icons/fa6";
-import { IoClose, IoPersonSharp } from "react-icons/io5";
+// import React, { useState } from "react";
+// import { FaBars } from "react-icons/fa6";
+// import { IoClose, IoPersonSharp } from "react-icons/io5";
+// import { Link, useNavigate } from "react-router-dom";
+// import { useAuth } from "../../context";
+// import Sidebar from "./Sidebar";
+
+// const Navbar = () => {
+//   const navigate = useNavigate();
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+//   const { isStudentLoggedIn, isMentorLoggedIn } = useAuth();
+
+//   const toggleSidebar = () => {
+//     setIsSidebarOpen(!isSidebarOpen);
+//   };
+
+//   const handleUserIconClick = () => {
+//     if (!isStudentLoggedIn && !isMentorLoggedIn) {
+//       navigate("/sign-up");
+//     } else if (isMentorLoggedIn || isStudentLoggedIn) {
+//       navigate("/dashboard");
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="bg-[#2e2e2e] sticky flex justify-between items-center w-full h-20 font-Poppins top-0 left-0 right-0 z-50 px-4">
+//         <div className="text-[#fefefe] text-2xl flex items-center cursor-pointer">
+//           {isSidebarOpen ? (
+//             <IoClose className="mr-2" onClick={toggleSidebar} />
+//           ) : (
+//             <FaBars className="mr-2" onClick={toggleSidebar} />
+//           )}
+//           <Link to="/" className="text-[#fefefe] text-2xl no-underline">
+//             MentorConnect
+//           </Link>
+//         </div>
+//         <div className="flex items-center gap-8">
+//           <Link
+//             to="/dashboard"
+//             className="text-[#fefefe] text-2xl no-underline"
+//           >
+//             Dashboard
+//           </Link>
+//           <Link to="/mentees" className="text-[#fefefe] text-2xl no-underline">
+//             My Mentees
+//           </Link>
+//           <Link to="/reports" className="text-[#fefefe] text-2xl no-underline">
+//             Reports
+//           </Link>
+//           <IoPersonSharp
+//             className="text-[#fefefe] cursor-pointer"
+//             size={24}
+//             onClick={handleUserIconClick}
+//           />
+//         </div>
+//       </div>
+//       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context";
+import { IoPersonSharp } from "react-icons/io5";
 import Sidebar from "./Sidebar";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const { isStudentLoggedIn, isMentorLoggedIn } = useAuth();
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
 
   const handleUserIconClick = () => {
     if (!isStudentLoggedIn && !isMentorLoggedIn) {
@@ -25,16 +83,9 @@ const Navbar = () => {
   return (
     <>
       <div className="bg-[#2e2e2e] sticky flex justify-between items-center w-full h-20 font-Poppins top-0 left-0 right-0 z-50 px-4">
-        <div className="text-[#fefefe] text-2xl flex items-center cursor-pointer">
-          {isSidebarOpen ? (
-            <IoClose className="mr-2" onClick={toggleSidebar} />
-          ) : (
-            <FaBars className="mr-2" onClick={toggleSidebar} />
-          )}
-          <Link to="/" className="text-[#fefefe] text-2xl no-underline">
-            MentorConnect
-          </Link>
-        </div>
+        <Link to="/" className="text-[#fefefe] text-2xl no-underline">
+          MentorConnect
+        </Link>
         <div className="flex items-center gap-8">
           <Link
             to="/dashboard"
@@ -55,7 +106,7 @@ const Navbar = () => {
           />
         </div>
       </div>
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <Sidebar />
     </>
   );
 };
