54708f56d44fad3556e9cdf02730d6b87a8410d8
Abdul Mannan <abdulmann1234@gmail.com>
2024-05-28 17:34:59 +0500

    Revised Navbar

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { IoPersonSharp } from "react-icons/io5";
-import { Link, useNavigate } from "react-router-dom";
+import { IoPersonSharp, IoSearch } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -16,40 +16,29 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 right-0 h-[10vh] bg-white border border-gray-200 flex justify-between items-center px-4 z-50 ml-[20vw]">
-      <Link
-        to="/"
-        className="text-black text-xl no-underline transition-colors duration-300 hover:text-green-500"
-      >
-        MentorConnect
-      </Link>
-      <div className="flex items-center gap-8">
-        <Link
-          to="/dashboard"
-          className="text-black text-xl no-underline transition-colors duration-300 hover:text-green-500"
-        >
-          Dashboard
-        </Link>
-        {isMentorLogin && (
-          <Link
-            to="/mentees"
-            className="text-black text-xl no-underline transition-colors duration-300 hover:text-green-500"
-          >
-            My Mentees
-          </Link>
-        )}
-        <Link
-          to="/reports"
-          className="text-black text-xl no-underline transition-colors duration-300 hover:text-green-500"
-        >
-          Reports
-        </Link>
-        <IoPersonSharp
-          className="cursor-pointer hover:text-green-500"
-          size={24}
-          onClick={handleUserIconClick}
+    <div className="fixed top-0 left-0 right-0 h-[10vh] bg-white border border-gray-200 flex justify-end items-center px-4 z-50 ml-[20vw] gap-16">
+      <div className="flex flex-end justify-center min-w-[350px]">
+        <div>
+          <div className="fixed">
+            <div className="h-full flex item-center justify-center">
+            <IoSearch className="text-[#00000052] absolute left-3 top-3" />
+            </div>
+          </div>
+        </div>
+
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search"
+          className="placeholder:text-[#00000052] bg-[#29affd18] p-2 rounded-[8px] pl-9 w-full font-Eczar"
         />
       </div>
+      <IoPersonSharp
+        className="cursor-pointer hover:text-green-500"
+        size={24}
+        onClick={handleUserIconClick}
+      />
     </div>
   );
 };
